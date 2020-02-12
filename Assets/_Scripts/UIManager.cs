0a47a6cc0ad8bcce4b31db44187344612ee2f41d
KaJuan Jones <47114033+kajuanjones@users.noreply.github.com>
2020-02-12 14:37:11 -0500

    Created basic menus
    Created basic menus and started on a new level.

---
+++ Assets/_Scripts/UIManager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UIManager : MonoBehaviour
+{
+    public void StartGame()
+    {
+        SceneManager.LoadScene("Level Select");
+    }
+
+    public void LevelSelect()
+    {
+        SceneManager.LoadScene("Game Play");
+    }
+}
+
+   
