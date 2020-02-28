c5829175f771e56a8d3eb9bb9fdbb227ee4245ec
KaJuan Jones <47114033+kajuanjones@users.noreply.github.com>
2020-02-28 15:06:01 -0500

    HealthBars
    Got names and health bars in. trying to figure out how to make them work.

---
+++ Assets/HealthBarTester.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthBarTester : MonoBehaviour
+{
+    private HealthBarTester healthBar;
+    
+    void Awake()
+    {
+        healthBar = GameObject.Find("HealthBar").GetComponent<HealthBarController>();
+
+    }
+
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            healthBar.changeHP(1);
+        }
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            healthBar.change(-1);
+        }
+    }
+}
