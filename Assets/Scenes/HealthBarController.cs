d4e7c2f989c59b2dffffe026012ddac0d1b436ee
KaJuan Jones <47114033+kajuanjones@users.noreply.github.com>
2020-03-11 13:54:55 -0400

    HealthBar working(I think)

---
+++ Assets/Scenes/HealthBarController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthBarController : MonoBehaviour
+{
+    public Slider HealthBar;
+    private float Health = 150f;
+
+    void Start()
+    {
+        HealthBar.maxValue = Health;
+    }
+
+    public void GetHit(float hitAmount)
+    {
+        Health -= hitAmount;
+        if (Health <= 0) Health = 0;
+        HealthBar.value = Health;
+    }
+}
